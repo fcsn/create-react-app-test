66f89effce4566656f7b2d9e4c1cc432e535ed01
sirfcsn <sirfcsn@gmail.com>
2018-11-16 13:03:08 +0900

    second commit for confirm remote repo

--- src/App.js
+++ src/App.js
@@ -3,6 +3,29 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor (props) {
+      super(props)
+      this.state = {
+          todoItems: [],
+          todoInput: '',
+          // todo 아이템이 들어갈 배열 선언
+      }
+      this._handleOnClickAddItem = this._handleOnClickAddItem.bind(this)
+      this._handleOnChangeTodoInput = this._handleOnChangeTodoInput.bind(this)
+  }
+    _handleOnClickAddItem () {
+      const {todoItems, todoInput} = this.state
+      if (todoItems.length === 0) return alert('???')
+      const todoItem = {
+        // id: todoId++,
+        title: todoInput,
+        isCompleted: false
+      }
+      return ''
+    }
+    _handleOnChangeTodoInput () {
+      return ''
+    }
   render() {
     return (
       <div className="App">
@@ -10,6 +33,8 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
+              <br/>
+              {this.state.todoItems}
           </p>
           <a
             className="App-link"
